a4587302148ad056d29c2b0d4b2a2766a7462c53
Björn Guth <bjoern@fsmpi.rwth-aachen.de>
2019-06-30 02:45:12 +0200

    Set resoapp at base level of the urls

--- resotool/resotool/urls.py
+++ resotool/resotool/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("resoapp/", include("resoapp.urls")),
+    path("", include("resoapp.urls")),
     path('admin/', admin.site.urls),
 ]
