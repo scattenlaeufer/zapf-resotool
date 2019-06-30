19f0b485420bbdcdf5a7a929c77f424924bbe7c6
Björn Guth <bjoern@fsmpi.rwth-aachen.de>
2019-06-30 02:47:18 +0200

    Create the first views to show resolutions
    They use Bootstrap, so it get added as well.

--- resotool/resoapp/urls.py
+++ resotool/resoapp/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
 
+from . import views
+
+app_name = "resoapp"
 urlpatterns = [
+    path("", views.ResolutionListView.as_view(), name="index"),
+    path("<int:pk>/", views.ResolutionView.as_view(), name="resolution"),
 ]
--- resotool/resoapp/views.py
+++ resotool/resoapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Resolution
+
+
+class ResolutionListView(generic.ListView):
+    def get_queryset(self):
+        return Resolution.objects.all().order_by("-date_submitted")
+
+
+class ResolutionView(generic.DetailView):
+    model = Resolution
