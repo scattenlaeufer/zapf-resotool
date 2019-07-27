a4c75d3658f87765e3d1e4618eb84bd84523daaf
Björn Guth <bjoern@fsmpi.rwth-aachen.de>
2019-07-27 15:32:41 +0200

    Add a first test for Resolution model

--- resotool/resoapp/tests/test_models.py
+++ resotool/resoapp/tests/test_models.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
-# Create your tests here.
+from resoapp import models
+
+
+class ResolutionTests(TestCase):
+
+    def test_basic_resolution_generation(self):
+        test_reso = models.Resolution(title="Testreso 1", reso_text="lorem ipsum")
+        test_reso.save()
+
+        self.assertIn(test_reso, models.Resolution.objects.all())
