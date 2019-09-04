b83622c9b2fd9e375ea5c3cd9d6227fcc6e8ff00
Björn Guth <bjoern@fsmpi.rwth-aachen.de>
2019-09-04 14:24:08 +0200

    Create some tests for ResolutionDetailView

--- resotool/resoapp/tests/test_views.py
+++ resotool/resoapp/tests/test_views.py
@@ -1,5 +1,24 @@
 from django.core import mail
 from django.test import TestCase
+from django.urls import reverse
+
+from resoapp import models, views
+
+
+class ResolutionDetailViewTests(TestCase):
+    def setUp(self):
+        self.reso = models.Resolution(title="Test Reso", reso_text="This is a test reso")
+        self.reso.save()
+
+    def test_view_returns_404(self):
+        response = self.client.get(reverse("resoapp:resolution", args=(self.reso.pk+1,)))
+        self.assertEqual(404, response.status_code)
+
+
+    def test_view_exists(self):
+        response = self.client.get(reverse("resoapp:resolution", args=(self.reso.pk,)))
+        self.assertEqual(200, response.status_code)
+        self.assertContains(response, self.reso.title)
 
 
 class SendResoTests(TestCase):
