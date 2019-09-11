31a52d1a325b20ae737ef17ecef2a64d9557d970
Joerg Behrmann <behrmann@physik.fu-berlin.de>
2019-09-11 15:20:45 +0200

    Add precommit black fixes

--- resotool/resoapp/tests/test_views.py
+++ resotool/resoapp/tests/test_views.py
@@ -7,13 +7,16 @@ from resoapp import models, views
 
 class ResolutionDetailViewTests(TestCase):
     def setUp(self):
-        self.reso = models.Resolution(title="Test Reso", reso_text="This is a test reso")
+        self.reso = models.Resolution(
+            title="Test Reso", reso_text="This is a test reso"
+        )
         self.reso.save()
 
     def test_view_returns_404(self):
-        response = self.client.get(reverse("resoapp:resolution", args=(self.reso.pk+1,)))
+        response = self.client.get(
+            reverse("resoapp:resolution", args=(self.reso.pk + 1,))
+        )
         self.assertEqual(404, response.status_code)
-
 
     def test_view_exists(self):
         response = self.client.get(reverse("resoapp:resolution", args=(self.reso.pk,)))
@@ -26,5 +29,7 @@ class SendResoTests(TestCase):
         pass
 
     def test_basic_send_mail(self):
-        mail.send_mail("Subject", "Message", "from@test.com", ["to@test.com"], fail_silently=False)
+        mail.send_mail(
+            "Subject", "Message", "from@test.com", ["to@test.com"], fail_silently=False
+        )
         self.assertEqual(len(mail.outbox), 1)
